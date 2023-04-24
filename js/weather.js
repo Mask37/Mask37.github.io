b535e43f6dc2fa14b15e6cc68dc629ef272e5cd4
Mask37 <306001576@qq.com>
2023-04-24 22:15:41 +0800

    Site updated: 2023-04-24 22:15:38

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "16",
+    "cityColor": "CCCCCC",
+    "citySize": "16",
+    "aqiColor": "D9D9D9",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "1250",
+    "top": "10",
+    "key": "814bb3dc4f3540efa710317eaf62b880"
+  }
+}
